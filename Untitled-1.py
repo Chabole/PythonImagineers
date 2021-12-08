f228302cf69d8923d91324986caaf314d44d0f44
Arthur Chabole <83458500+Chabole@users.noreply.github.com>
2021-12-08 20:27:55 -0300

    Add __str__()

--- Untitled-1.py
+++ Untitled-1.py
@@ -2,10 +2,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 class teste:
-        def __init__(self, nome, idade):
+    def __init__(self, nome, idade):
         self.nome = nome
         self.idade = idade
 
+    def __str__(self):
+        print(f'Meu nome é {self.nome} e tenho {self.idade} anos')
 
 
 
