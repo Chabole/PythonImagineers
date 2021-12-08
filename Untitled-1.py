81b1a2739f5d0d413f6fb833e1bed121515fb7b4
Arthur Chabole <83458500+Chabole@users.noreply.github.com>
2021-12-08 20:36:37 -0300

    Add ano_nascimento()

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-class teste:
+class Pessoa:
     def __init__(self, nome, idade):
         self.nome = nome
         self.idade = idade
@@ -9,9 +9,9 @@ class teste:
     def __str__(self):
         print(f'Meu nome é {self.nome} e tenho {self.idade} anos')
 
-
-
-
-        
-
-
+    def ano_nascimento(self, ano_atual):
+        ano = ano_atual - self.idade
+        print(f'Vc nasceu no ano de {ano}')
+  
+objects = Pessoa('Arthur', 22)
+print('Done!')
